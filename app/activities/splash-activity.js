b8e9470d6e9d8ef7d2383bea24ed488dcb8d8ed0
Luca Nicolini <mithosk@alice.it>
2021-01-07 15:46:04 +0100

    remove splash activity

--- app/activities/splash-activity.js
+++ app/activities/splash-activity.js
@@ -3,13 +3,14 @@
 import { style } from '../style';
 import React, { Component } from 'react';
 import { View, Image } from 'react-native';
+import { CommonActions } from '@react-navigation/native';
 
 export default class SplashActivity extends Component {
 
   constructor() {
     super();
 
-    setTimeout(() => this.props.navigation.navigate('home'), 4000);
+    this.Startup();
   }
 
   render() {
@@ -18,6 +19,23 @@ export default class SplashActivity extends Component {
         <Image source={require('../images/logo.png')} style={{ width: 200, height: 200 }} />
       </View>
     );
+  }
+
+  Startup = () => {
+    setTimeout(() => {
+      this.props.navigation.navigate('home');
+
+      this.props.navigation.dispatch(s => {
+        var routes = s.routes.filter(r => r.name !== 'splash');
+
+        return CommonActions.reset({
+          ...s,
+          routes,
+          index: routes.length - 1,
+        });
+      });
+    },
+      4000);
   }
 
 }
