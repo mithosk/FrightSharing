48575e7641565df12f67dba091595112206d58aa
Luca Nicolini <mithosk@alice.it>
2021-01-19 11:40:28 +0100

    setted icons

--- index.js
+++ index.js
@@ -2,18 +2,19 @@
 
 import React from 'react';
 import { style } from './app/style';
+import { Icon } from 'react-native-elements'
 import { name, displayName } from './app.json';
 import HomeActivity from './app/activities/home-activity';
-import { AppRegistry, StatusBar, Text } from 'react-native';
 import SplashActivity from './app/activities/splash-activity';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { AppRegistry, StatusBar, Text, View } from 'react-native';
 
 const Stack = createStackNavigator();
 
 AppRegistry.registerComponent(name, () => () => (
   <NavigationContainer>
-    <StatusBar hidden={false} backgroundColor={style.StatusBar.backgroundColor} barStyle="dark-content" />
+    <StatusBar backgroundColor={style.StatusBar.backgroundColor} barStyle="dark-content" />
     <Stack.Navigator>
       <Stack.Screen name="splash" component={SplashActivity} options=
         {
@@ -27,7 +28,17 @@ AppRegistry.registerComponent(name, () => () => (
             headerStyle: {
               backgroundColor: style.Header.backgroundColor
             },
-            headerTitle: <Text style={{ fontFamily: 'Spicy Pumpkin', fontSize: 25 }}>{displayName}</Text>
+            headerTitle: <Text style={style.HeaderTitle}>{displayName}</Text>,
+            headerLeft: () => (
+              <View style={{ width: 60 }}>
+                <Icon type="feather" name="search" onPress={() => alert("SEARCH")} />
+              </View>
+            ),
+            headerRight: () => (
+              <View style={{ width: 60 }}>
+                <Icon type="feather" name="edit-3" onPress={() => alert("WRITE")} />
+              </View>
+            )
           }
         } />
     </Stack.Navigator>
