d47dfa8bde0ca43e85ba3b7575855af5888f4b51
Luca Nicolini <mithosk@alice.it>
2021-02-03 17:55:13 +0100

    modal buttons

--- app/activities/home-activity.js
+++ app/activities/home-activity.js
@@ -1,9 +1,10 @@
 'use strict';
 
 import { style } from '../style';
+import { View } from 'react-native';
 import Modal from 'react-native-modal';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Icon, Button } from 'react-native-elements';
 
 export default class HomeActivity extends Component {
 
@@ -16,7 +17,18 @@ export default class HomeActivity extends Component {
       <View>
         <Modal isVisible={this.props.route.params.searchModal} animationInTiming={1} animationOutTiming={1}>
           <View style={style.SearchModal}>
-            <Text onPress={() => this.props.navigation.navigate('home', { searchModal: false })}>Leonardo Nicolini Bello!!</Text>
+            <View style={{ width: "90%", marginTop: 0 }}>
+              <Button buttonStyle={style.Button} icon={<Icon type="feather" name="clock" size={45} />} title="LE NUOVE STORIE" titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "new" })} />
+            </View>
+            <View style={{ width: "90%", marginTop: 10 }}>
+              <Button buttonStyle={style.Button} icon={<Icon type="feather" name="thumbs-up" size={45} />} title="I MIGLIORI SPAVENTI" titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "best" })} />
+            </View>
+            <View style={{ width: "90%", marginTop: 10 }}>
+              <Button buttonStyle={style.Button} icon={<Icon type="feather" name="map-pin" size={45} />} title="ESPERIENZE VICINE A ME" titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "near" })} />
+            </View>
+            <View style={{ width: "90%", marginTop: 10 }}>
+              <Button buttonStyle={style.Button} icon={<Icon type="feather" name="user" size={45} />} title="I MIEI RACCONTI" titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "mine" })} />
+            </View>
           </View>
         </Modal>
       </View>
