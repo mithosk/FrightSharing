2c6e474ca07addc4bd53291e3bc04fa12f6b578d
Luca Nicolini <mithosk@alice.it>
2021-02-16 10:59:53 +0100

    add entry to vocabulary

--- app/vocabulary.js
+++ app/vocabulary.js
@@ -4,12 +4,18 @@ import I18n from 'react-native-i18n';
 
 I18n.translations = {
     it: {
+        addstory_header_title: "Nuova storia",
+        addstory_body_publish: "Pubblica questa storia",
+
         home_search_new: "Le nuove storie",
         home_search_best: "I migliori spaventi",
         home_search_near: "Esperienze nei dintorni",
         home_search_mine: "I miei racconti"
     },
     en: {
+        addstory_header_title: "New story",
+        addstory_body_publish: "Publish this story",
+
         home_search_new: "New stories",
         home_search_best: "The best frights",
         home_search_near: "Experiences around here",
