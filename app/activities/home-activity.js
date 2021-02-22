46bc949812f47470b1861fdedc7db759fc54c8c9
Luca Nicolini <mithosk@alice.it>
2021-02-22 11:32:58 +0100

    home restiling

--- app/activities/home-activity.js
+++ app/activities/home-activity.js
@@ -16,18 +16,18 @@ export default class HomeActivity extends Component {
   render() {
     return (
       <Modal isVisible={this.props.route.params.searchModal} animationInTiming={1} animationOutTiming={1}>
-        <View style={style.SearchModal}>
-          <View style={{ width: "90%", marginTop: 0 }}>
-            <Button buttonStyle={style.Button} icon={<Icon type="feather" name="clock" size={45} />} title={translate("home_search_new")} titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "new" })} />
+        <View style={style.HomeSearchModal}>
+          <View style={{ width: "90%", marginTop: 1 }}>
+            <Button buttonStyle={style.ButtonLight} icon={<Icon type="feather" name="clock" size={45} />} title={translate("home_search_new")} titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "new" })} />
           </View>
           <View style={{ width: "90%", marginTop: 10 }}>
-            <Button buttonStyle={style.Button} icon={<Icon type="feather" name="thumbs-up" size={45} />} title={translate("home_search_best")} titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "best" })} />
+            <Button buttonStyle={style.ButtonLight} icon={<Icon type="feather" name="thumbs-up" size={45} />} title={translate("home_search_best")} titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "best" })} />
           </View>
           <View style={{ width: "90%", marginTop: 10 }}>
-            <Button buttonStyle={style.Button} icon={<Icon type="feather" name="map-pin" size={45} />} title={translate("home_search_near")} titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "near" })} />
+            <Button buttonStyle={style.ButtonLight} icon={<Icon type="feather" name="map-pin" size={45} />} title={translate("home_search_near")} titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "near" })} />
           </View>
           <View style={{ width: "90%", marginTop: 10 }}>
-            <Button buttonStyle={style.Button} icon={<Icon type="feather" name="user" size={45} />} title={translate("home_search_mine")} titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "mine" })} />
+            <Button buttonStyle={style.ButtonLight} icon={<Icon type="feather" name="user" size={45} />} title={translate("home_search_mine")} titleStyle={style.ButtonTitle} onPress={() => this.props.navigation.navigate("home", { searchModal: false, searchMode: "mine" })} />
           </View>
         </View>
       </Modal>
