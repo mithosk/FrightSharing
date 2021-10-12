1f1319ab7d44e91452fc886a6412b5dc2004041a
Luca Nicolini <mithosk85@gmail.com>
2021-10-12 13:07:26 +0200

    internal storage repository for stories

--- app/repositories/story-repository.js
+++ app/repositories/story-repository.js
@@ -1,5 +1,38 @@
 'use strict';
 
-module.exports.post = async function (data) {
-    alert(JSON.stringify(data));
+import createGuid from 'react-native-create-guid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+module.exports.post = async function (body) {
+    body.id = await createGuid();
+    body.date = new Date();
+
+    var stories = [];
+    var json = await AsyncStorage.getItem("stories");
+    if (json != null)
+        stories = JSON.parse(json);
+
+    stories.push(body);
+
+    await AsyncStorage.setItem("stories", JSON.stringify(stories));
+}
+
+module.exports.list = async function (filter) {
+    var stories = [];
+    var json = await AsyncStorage.getItem("stories");
+    if (json != null)
+        stories = JSON.parse(json);
+
+    var result = [];
+    var from = (filter.pageIndex - 1) * filter.pageSize;
+    if (from < stories.length) {
+        var to = from + filter.pageSize - 1;
+        if (to >= stories.length)
+            to = stories.length - 1;
+
+        for (var i = from; i <= to; i++)
+            result.push(stories[i]);
+    }
+
+    return result;
 }
